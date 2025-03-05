efdd8769df4abaf1eac790e433638abedbd1a35a
Roni <ronniiiip@gmail.com>
2025-03-05 15:18:47 +0000

    all

---
+++ constants/Fonts.js
@@ -0,0 +1,6 @@
+// fonts.js
+export const fonts = {
+  ArchitectsDaughterRegular: require("../assets/fonts/ArchitectsDaughter-Regular.ttf"),
+  Alegreya: require("../assets/fonts/Alegreya-VariableFont_wght.ttf"),
+  Montserrat: require("../assets/fonts/Montserrat-VariableFont_wght.ttf"),
+};
